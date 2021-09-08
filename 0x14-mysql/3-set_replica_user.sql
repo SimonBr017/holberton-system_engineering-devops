1c40296810897727aaa5993b0e971b19c84e03f1
SimonBr017 <simonbrard017@gmail.com>
2021-09-08 19:13:09 +0200

    add t4

--- 0x14-mysql/3-set_replica_user.sql
+++ 0x14-mysql/3-set_replica_user.sql
@@ -1,6 +1,6 @@
 -- set up a replica_user
 
-CREATE USER 'replica_user'@'localhost' IDENTIFIED BY 'C3s2l"m3r2';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'localhost';
+CREATE USER 'replica_user'@'%' IDENTIFIED BY 'C3s2l"m3r2';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
