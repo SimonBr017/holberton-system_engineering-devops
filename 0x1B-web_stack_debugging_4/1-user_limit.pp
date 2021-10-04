782d2d4339a72e45e4f4a88ce027d7bf97f83097
SimonBr017 <simonbrard017@gmail.com>
2021-10-04 14:33:30 +0200

    add advance

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# increase hard and soft file limit for holberton 
+
+exec {'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec {'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
